7cbd93c3bdeb6706e83250358ad66da974843905
oatspear <oatspear@gmail.com>
2023-01-17 14:51:00 +0000

    Update client.js

--- client.js
+++ client.js
@@ -107,18 +107,3 @@ app.component("BattlePlayerInfo", BattlePlayerInfo);
 app.component("BattleIconLabel", BattleIconLabel);
 
 app.mount("#app");
-
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
-
-// nextButton.addEventListener("click", () => {
-//   const slideWidth = slide.clientWidth;
-//   slidesContainer.scrollLeft += slideWidth;
-// });
-//
-// prevButton.addEventListener("click", () => {
-//   const slideWidth = slide.clientWidth;
-//   slidesContainer.scrollLeft -= slideWidth;
-// });
